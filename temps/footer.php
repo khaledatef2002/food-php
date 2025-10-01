97c12c608c5414121b171be32184d32d34361910
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2025-10-02 00:39:47 +0300

    hide footer visa logos when pay by visa not enabled

--- temps/footer.php
+++ temps/footer.php
@@ -1,15 +1,16 @@
-<!-- <div><bdi>Copyright Diafh &copy;</bdi></div> -->
- <ul class="d-flex gap-2 mb-0" style="list-style: none;">
-    <li>
-    <img src="imgs/signs/master_card_logo.png" height="20px">
-    </li>
-    <li>
-    <img src="imgs/signs/meeza_logo.png" height="20px">
-    </li>
-    <li>
-    <img src="imgs/signs/qnb_logo.svg" height="20px">
-    </li>
-    <li>
-    <img src="imgs/signs/visa_logo.png" height="20px">
-    </li>
-</ul>
+<?php if($is_visa_available): ?>
+    <ul class="d-flex gap-2 mb-0" style="list-style: none;">
+        <li>
+        <img src="imgs/signs/master_card_logo.png" height="20px">
+        </li>
+        <li>
+        <img src="imgs/signs/meeza_logo.png" height="20px">
+        </li>
+        <li>
+        <img src="imgs/signs/qnb_logo.svg" height="20px">
+        </li>
+        <li>
+        <img src="imgs/signs/visa_logo.png" height="20px">
+        </li>
+    </ul>
+<?php endif; ?>
